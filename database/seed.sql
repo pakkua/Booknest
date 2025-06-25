fa630c8c471b567f4142436995906011e2c59751
pakkua <b22cn066@kitsw.ac.in>
2025-06-25 08:36:30 +0530

    Add files via upload

---
+++ database/seed.sql
@@ -0,0 +1,13 @@
+USE booknest;
+
+INSERT INTO books (title, price) VALUES
+('The Alchemist', 299.00),
+('Atomic Habits', 449.00),
+('Wings of Fire', 350.00),
+('Sherlock Holmes', 399.00),
+('Data Structures in C', 559.00),
+('Indian Polity by Laxmikant', 499.00);
+
+INSERT INTO users (email, password) VALUES
+('admin@example.com', 'admin123'),
+('user@example.com', 'user123');
